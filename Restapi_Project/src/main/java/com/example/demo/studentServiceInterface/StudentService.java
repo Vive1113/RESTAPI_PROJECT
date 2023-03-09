e41651adc9268d938d6a635397ea4963accd3aed
Vive1113 <120009949+Vive1113@users.noreply.github.com>
2023-03-09 21:34:01 +0530

    initial commit

---
+++ Restapi_Project/src/main/java/com/example/demo/studentServiceInterface/StudentService.java
@@ -0,0 +1,16 @@
+package com.example.demo.studentServiceInterface;
+import java.util.List;
+import com.example.demo.studentClass.Students;
+
+/* Student service interface have only method declaration and 
+ * it implements in student service implementation class */
+
+public interface StudentService {
+	Students saveStudent(Students student);
+	List<Students>  getStudentsFromDb();
+	Students getStudentById(int studentId);
+	Students updateStudentDetails(Students student, int studentId);
+	void deleteStudentById(int studentId);
+}
+
+
